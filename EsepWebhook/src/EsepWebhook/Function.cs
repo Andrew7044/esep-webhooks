cb69a06d89ee6a9a7187c8e16bce58d503e1e743
Andrew7044 <80797345+Andrew7044@users.noreply.github.com>
2024-04-02 23:11:14 -0400

    Update Function.cs

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        ynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
         
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
         
